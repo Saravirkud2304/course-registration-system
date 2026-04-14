47cd6b2f9b88298469603f4fe18da9a280729704
Sara Virkud <sara.virkud@gmail.com>
2026-04-14 15:03:04 +0530

    Final cleanup with gitignore

--- bin/test_mysql.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Testing MySQL installation...
-mysql --version
-if %ERRORLEVEL% EQU 0 (
-    echo MySQL is installed and working!
-) else (
-    echo MySQL is not installed or not in PATH
-)
-pause
